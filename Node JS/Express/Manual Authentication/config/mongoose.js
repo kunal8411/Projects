2a472497daf539591577b0d07e7f4ac6d796cf20
kunal8411 <63067292+kunal8411@users.noreply.github.com>
2020-07-07 14:53:38 +0530

    Create mongoose.js

---
+++ Node JS/Express/Manual Authentication/config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose= require('mongoose');
+
+
+// create a database having name codeais-developnment 
+mongoose.connect('mongodb://localhost/codeail-development');
+
+
+
+//Acquire the connection:-check whether db is  connected or not
+const db= mongoose.connection;
+
+//error
+db.on('error',console.error.bind(console,'erroro connecting to database '));
+
+//if up and running
+db.once('open',function(){
+    console.log(" database connected successfully");
+    
+})
+

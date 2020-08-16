2529f0b71642b5dcf3a5e7d0cb5867941c8fd7c2
kunal8411 <63067292+kunal8411@users.noreply.github.com>
2020-08-16 12:45:56 +0530

    Create category_controller.js

---
+++ Food Delivery Application/controllers/category_controller.js
@@ -0,0 +1,37 @@
+module.exports.fruits= function(req,res){
+     res.render('fruits');
+}
+
+module.exports.vegetables= function(req,res){
+     res.render('vegetables');
+
+}
+
+module.exports.dailyneeds= function(req,res){
+     res.render('dailyneeds');
+
+}
+module.exports.milk= function(req,res){
+     res.render('milk');
+
+}
+module.exports.dairyproducts= function(req,res){
+     res.render('dairyproducts');
+}
+module.exports.bakery= function(req,res){
+     res.render('bakerybisc');
+}
+module.exports.beverages= function(req,res){
+     res.render('beverages');
+}
+module.exports.snacks= function(req,res){
+     res.render('snacks');
+}
+module.exports.breakfast= function(req,res){
+     res.render('breakfast');
+}
+module.exports.spreads= function(req,res){
+     res.render('spreads');
+}
+
+
